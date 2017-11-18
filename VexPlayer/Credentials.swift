b8485b79de0ade4d25c73944cb0ac753a644fbfa
Gustavo <prercursor@gmail.com>
2017-11-18 16:11:53 -0200

    add gitignore, credentials

---
+++ VexPlayer/Credentials.swift
@@ -0,0 +1,18 @@
+//
+//  Credentials.swift
+//  VexPlayer
+//
+//  Created by Gustavo Carvalho Costa on 18/11/17.
+//  Copyright © 2017 Gustavo Carvalho Costa. All rights reserved.
+//
+
+import Foundation
+
+
+
+struct Credentials {
+    private struct Spotify {
+        static let clientSecret = ""
+        static let clientId = ""
+    }
+}
